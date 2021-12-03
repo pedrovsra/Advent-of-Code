eb8f0010bf4fe4de4d0efe76f2dae89f8d5b835c
Pedro Tiago Araujo <ptvsaraujo@gmail.com>
2021-12-03 13:26:51 +0800

    Update part1.cpp

--- day2/part1.cpp
+++ day2/part1.cpp
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+using namespace std;
 
+const char FORWARD[] = "forward";
+const char DOWN[] = "down";
+const char UP[] = "up";
+
+char command[10];
+int unit = 0;
+
+int position = 0;
+int depth = 0;
+
+int main() {
+
+  FILE* f = fopen("input.txt", "r");
+
+  while(fscanf(f, "%s %d", command, &unit) > 0) {
+    if (strcmp(command, FORWARD) == 0) position += unit;
+    else if (strcmp(command, DOWN) == 0) depth += unit;
+    else if (strcmp(command, UP) == 0) depth -= unit;
+  }
+
+  printf("%d\n", (position * depth));
+
+  return 0;
+}
